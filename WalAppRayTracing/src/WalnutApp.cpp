8f29c47782902364e43a67d2ec1340fc0d2337cd
AndreaMas <andrea.masciulli@me.com>
2024-02-24 14:02:34 +0100

    window: basic viewport and settings windows implemented, viewport renders pink screen

--- WalAppRayTracing/src/WalnutApp.cpp
+++ WalAppRayTracing/src/WalnutApp.cpp
@@ -3,36 +3,86 @@
 
 #include "Walnut/Image.h"
 
-class ExampleLayer : public Walnut::Layer
-{
-public:
-	virtual void OnUIRender() override
-	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
-		ImGui::End();
+using namespace Walnut;
 
-		ImGui::ShowDemoWindow();
-	}
-};
+//class ExampleLayer : public Walnut::Layer
+//{
+//public:
+//
+//	virtual void OnUIRender() override
+//	{
+//		ImGui::Begin("Hello");
+//		ImGui::Button("Button");
+//		ImGui::End();
+//
+//		ImGui::ShowDemoWindow();
+//	}
+//};
 
 class MasLayer : public Walnut::Layer
 {
 public:
+
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Mas");
-		ImGui::Button("MasMas");
+		// settings panel
+
+		ImGui::Begin("Settings");
+
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
+
 		ImGui::End();
 
-		//ImGui::ShowDemoWindow();
+		// viewport panel
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		// display/render image {this sets size in floats}
+
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
+		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	void Render()
+	{
+		if (!m_Image || 
+			m_ViewportWidth != m_Image->GetWidth() || 
+			m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) 
+		{
+			m_ImageData[i] = 0xffff00ff;
+		}
+
+		m_Image->SetData(m_ImageData);
+	}
+
+private:
+
+	std::shared_ptr<Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "Ray Tracing";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	//app->PushLayer<ExampleLayer>();
@@ -48,5 +98,6 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 			ImGui::EndMenu();
 		}
 	});
+
 	return app;
 }
